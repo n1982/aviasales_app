e479890709fb021a18acb00abeb1b297e05f5ec7
nick selivanov <nick.selivanov@gmail.com>
2021-12-13 12:47:00 +0300

    style checkbox

--- src/components/Filter/Filter.jsx
+++ src/components/Filter/Filter.jsx
@@ -5,31 +5,36 @@ import styles from './Filter.module.scss';
 const Filter = () => (
   <div className={styles.filter}>
     <div className={styles.title}>КОЛИЧЕСТВО ПЕРЕСАДОК</div>
-
-    <input type="checkbox" id="all" className={styles.checkbox} />
-    <label className={styles.label} htmlFor="all">
-      Все
-    </label>
-
-    <input type="checkbox" id="0 direct" className={styles.checkbox} />
-    <label className={styles.label} htmlFor="0 direct">
-      Без пересадок
-    </label>
-
-    <input type="checkbox" id="1 direct" className={styles.checkbox} />
-    <label className={styles.label} htmlFor="1 direct">
-      1 пересадка
-    </label>
-
-    <input type="checkbox" id="2 direct" className={styles.checkbox} />
-    <label className={styles.label} htmlFor="2 direct">
-      2 пересадки
-    </label>
-
-    <input type="checkbox" id="3 direct" className={styles.checkbox} />
-    <label className={styles.label} htmlFor="3 direct">
-      3 пересадки
-    </label>
+    <div className={styles.wrapper}>
+      <input type="checkbox" id="all" className={styles.checkbox} />
+      <label className={styles.label} htmlFor="all">
+        Все
+      </label>
+    </div>
+    <div className={styles.wrapper}>
+      <input type="checkbox" id="0 direct" className={styles.checkbox} />
+      <label className={styles.label} htmlFor="0 direct">
+        Без пересадок
+      </label>
+    </div>
+    <div className={styles.wrapper}>
+      <input type="checkbox" id="1 direct" className={styles.checkbox} />
+      <label className={styles.label} htmlFor="1 direct">
+        1 пересадка
+      </label>
+    </div>
+    <div className={styles.wrapper}>
+      <input type="checkbox" id="2 direct" className={styles.checkbox} />
+      <label className={styles.label} htmlFor="2 direct">
+        2 пересадки
+      </label>
+    </div>
+    <div className={styles.wrapper}>
+      <input type="checkbox" id="3 direct" className={styles.checkbox} />
+      <label className={styles.label} htmlFor="3 direct">
+        3 пересадки
+      </label>
+    </div>
   </div>
 );
 export default Filter;
